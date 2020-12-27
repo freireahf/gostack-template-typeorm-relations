b5b8f9b3811dcaae89face6d379a76f2526b4aef
Hugo Freire <h-f@outlook.com.br>
2020-12-26 22:33:44 -0300

    :sparkles: challage finalize

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
@@ -6,6 +6,15 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createCustomer = container.resolve(CreateCustomerService);
+
+    const customer = await createCustomer.execute({
+      name,
+      email,
+    });
+
+    return response.json(customer);
   }
 }
